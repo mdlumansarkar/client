2249cfa99d01289217c21424139e9fdfec7a7c94
mdlumansarkar <122555404+mdlumansarkar@users.noreply.github.com>
2025-01-30 20:15:07 +0600

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,7 @@ const Contact = () => {
   // Fetch user data
   const fetchUserData = async () => {
     try {
-      const response = await fetch("https://mern-project-3-cg8m.onrender.com/getData", {
+      const response = await fetch("https://mern-project-3-cg8m.onrender.com/contact", {
         method: "GET",
         headers: {
           // Accept: "application/json", Here not cookies send
